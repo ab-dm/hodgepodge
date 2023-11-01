55b63df80d71d1e26f2170d630163dffefa58a06
Dmitry Abramov <abramov.dmittry@gmail.com>
2023-11-01 19:26:33 +0300

    Add project files.

---
+++ Baucenter.ru/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Baucenter.ru
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
